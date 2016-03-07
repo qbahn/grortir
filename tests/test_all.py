edac50c2c38ec325341e00aed7c2a224b7d674c9
Wojtek <wojtekpietrucha@gmail.com>
2016-03-07 20:06:03 +0100

    Fix PR

--- tests/test_all.py
+++ tests/test_all.py
@@ -10,8 +10,8 @@ class TestGrortir(unittest.TestCase):
 
     """Sample integration test class."""
 
-    # def test_network_stuff(self):
-    #     assert sample.function_with_network_stuff() is True
+    def test_network_stuff(self):
+        assert sample.function_with_network_stuff() is True
 
     def test_disk_stuff(self):
         assert sample.function_with_disk_stuff() is False
