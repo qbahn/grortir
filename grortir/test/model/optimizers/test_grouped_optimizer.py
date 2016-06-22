7054cfd113c1d14ea532e02ff5a3d1230579824d
Wojtek <wojtekpietrucha@gmail.com>
2016-06-23 00:45:55 +0200

    With that files we are able to run tests via PyCharm. Added last file.

---
+++ grortir/test/model/optimizers/test_grouped_optimizer.py
@@ -0,0 +1,29 @@
+"""Module for testing grouped optimizer."""
+
+from unittest import TestCase
+from unittest.mock import Mock
+
+from grortir.main.model.core.abstract_process import AbstractProcess
+from grortir.main.model.core.abstract_stage import AbstractStage
+from grortir.main.optimizers.grouped_optimizer import GroupedOptimizer
+
+
+class TestGroupedOptimizer(TestCase):
+    """Class for testing Optimizer."""
+
+    def setUp(self):
+        """Set up environment."""
+        self.some_process = AbstractProcess()
+        self.first_stage = AbstractStage()
+        self.second_stage = AbstractStage()
+        self.third_stage_a = Mock()
+        self.third_stage_b = Mock()
+        self.some_process.add_path([self.first_stage, self.second_stage,
+                                    self.third_stage_a])
+        self.some_process.add_edge(self.second_stage, self.third_stage_b)
+
+    def test___init__(self):
+        """Testing creating object."""
+        optimizer = GroupedOptimizer(self.some_process)
+        self.assertIsNotNone(optimizer)
+        self.assertEqual(optimizer.process, self.some_process)
