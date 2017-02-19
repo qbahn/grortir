376942e85bf0e1d2bbb7221644ed08f122ee06d7
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-02-19 23:53:23 +0100

    Implement script with unbalanced and balnaced trees.

--- grortir/scripts/trees_simulations.py
+++ grortir/scripts/trees_simulations.py
@@ -40,7 +40,7 @@ def calculate_probability_of_success(max_calls, number_of_nodes,
 
 
 MAX_CALLS = 1000
-EXPECTED_QUALITY = 3.8
+EXPECTED_QUALITY = 0.001
 INPUT_VECTOR = (0,)
 HOW_MANY_PARTICLES = 40
 HOW_MANY_TRIES = 100
@@ -71,7 +71,8 @@ def create_edges_unbalanced(process, stages):
     process.add_edge(stages[10], stages[13])
     process.add_edge(stages[10], stages[14])
 
-def create_PSO_algorithm_BFS_SEQ():
+
+def create_PSO_algorithm_BFS_balanced_SEQ():
     bfs_stages = create_stages()
     bfs_process = CallsProcess()
     create_edges_balanced(bfs_process, bfs_stages)
@@ -84,7 +85,7 @@ def create_PSO_algorithm_BFS_SEQ():
                         bfs_optimization_strategy, HOW_MANY_PARTICLES)
 
 
-def create_PSO_algorithm_BFS_SIM():
+def create_PSO_algorithm_BFS_balanced_SIM():
     bfs_stages = create_stages()
     bfs_process = CallsProcess()
     create_edges_balanced(bfs_process, bfs_stages)
@@ -99,27 +100,11 @@ def create_PSO_algorithm_BFS_SIM():
 # BFS part
 
 
-def create_PSO_algorithm_DFS_SEQ():
+def create_PSO_algorithm_DFS_balanced_SEQ():
     dfs_stages = create_stages()
     dfs_process = CallsProcess()
     create_edges_balanced(dfs_process, dfs_stages)
-    dfs_ordered_stages = [
-        dfs_stages[0],
-        dfs_stages[1],
-        dfs_stages[3],
-        dfs_stages[7],
-        dfs_stages[8],
-        dfs_stages[4],
-        dfs_stages[9],
-        dfs_stages[10],
-        dfs_stages[2],
-        dfs_stages[5],
-        dfs_stages[11],
-        dfs_stages[12],
-        dfs_stages[6],
-        dfs_stages[13],
-        dfs_stages[14]
-    ]
+    dfs_ordered_stages = get_dfs_balanced_ordered_stages(dfs_stages)
     dfs_grouping_strategy = GroupingStrategy(dfs_ordered_stages)
     for stage in dfs_ordered_stages:
         dfs_grouping_strategy.define_group([stage])
@@ -128,62 +113,112 @@ def create_PSO_algorithm_DFS_SEQ():
                         dfs_optimization_strategy, HOW_MANY_PARTICLES)
 
 
-def create_PSO_algorithm_DFS_SIM():
+def create_PSO_algorithm_DFS_unbalanced_SEQ():
     dfs_stages = create_stages()
     dfs_process = CallsProcess()
-    create_edges_balanced(dfs_process, dfs_stages)
-    dfs_ordered_stages = [
-        dfs_stages[0],
-        dfs_stages[1],
-        dfs_stages[3],
-        dfs_stages[7],
-        dfs_stages[8],
-        dfs_stages[4],
-        dfs_stages[9],
-        dfs_stages[10],
-        dfs_stages[2],
-        dfs_stages[5],
-        dfs_stages[11],
-        dfs_stages[12],
-        dfs_stages[6],
-        dfs_stages[13],
-        dfs_stages[14]
-    ]
+    create_edges_unbalanced(dfs_process, dfs_stages)
+    dfs_ordered_stages = get_dfs_unbalanced_ordered_stages(dfs_stages)
     dfs_grouping_strategy = GroupingStrategy(dfs_ordered_stages)
-    dfs_grouping_strategy.define_group(dfs_ordered_stages)
+    for stage in dfs_ordered_stages:
+        dfs_grouping_strategy.define_group([stage])
     dfs_optimization_strategy = CallsOptimizationStrategy()
     return PsoAlgorithm(dfs_process, dfs_grouping_strategy,
                         dfs_optimization_strategy, HOW_MANY_PARTICLES)
 
 
+def create_PSO_algorithm_DFS_balanced_SIM():
+    dfs_stages = create_stages()
+    dfs_process = CallsProcess()
+    create_edges_balanced(dfs_process, dfs_stages)
+    dfs_ordered_stages_balanced = get_dfs_balanced_ordered_stages(dfs_stages)
+    dfs_grouping_strategy = GroupingStrategy(dfs_ordered_stages_balanced)
+    dfs_grouping_strategy.define_group(dfs_ordered_stages_balanced)
+    dfs_optimization_strategy = CallsOptimizationStrategy()
+    return PsoAlgorithm(dfs_process, dfs_grouping_strategy,
+                        dfs_optimization_strategy, HOW_MANY_PARTICLES)
+
+
+def create_PSO_algorithm_DFS_unbalanced_SIM():
+    dfs_stages = create_stages()
+    dfs_process = CallsProcess()
+    create_edges_unbalanced(dfs_process, dfs_stages)
+    dfs_ordered_stages_unbalanced = get_dfs_unbalanced_ordered_stages(
+        dfs_stages)
+    dfs_grouping_strategy = GroupingStrategy(dfs_ordered_stages_unbalanced)
+    dfs_grouping_strategy.define_group(dfs_ordered_stages_unbalanced)
+    dfs_optimization_strategy = CallsOptimizationStrategy()
+    return PsoAlgorithm(dfs_process, dfs_grouping_strategy,
+                        dfs_optimization_strategy, HOW_MANY_PARTICLES)
+
+
+def get_dfs_balanced_ordered_stages(stages):
+    return [
+        stages[0],
+        stages[1],
+        stages[3],
+        stages[7],
+        stages[8],
+        stages[4],
+        stages[9],
+        stages[10],
+        stages[2],
+        stages[5],
+        stages[11],
+        stages[12],
+        stages[6],
+        stages[13],
+        stages[14]
+    ]
+
+
+def get_dfs_unbalanced_ordered_stages(stages):
+    return [
+        stages[0],
+        stages[1],
+        stages[3],
+        stages[4],
+        stages[2],
+        stages[5],
+        stages[7],
+        stages[8],
+        stages[6],
+        stages[9],
+        stages[11],
+        stages[12],
+        stages[10],
+        stages[13],
+        stages[14]
+    ]
+
+
 # DFS part
 dfs_results = []
-dfs_success_count_SIM = 0
-bfs_success_count_SIM = 0
-dfs_success_count_SEQ = 0
-bfs_success_count_SEQ = 0
+dfs_success_balanced_count_SIM = 0
+dfs_unbalanced_success_count_SIM = 0
+dfs_balanced_success_count_SEQ = 0
+dfs_unbalanced_success_count_SEQ = 0
 for i in range(HOW_MANY_TRIES):
-    dfs_pso_SIM = create_PSO_algorithm_DFS_SIM()
-    dfs_pso_SIM.run()
-    if dfs_pso_SIM.process.optimization_status == OptimizationStatus.success:
-        dfs_success_count_SIM += 1
-        print("DFS SIM SUCCESS!")
-    bfs_pso_SIM = create_PSO_algorithm_BFS_SIM()
-    bfs_pso_SIM.run()
-    if bfs_pso_SIM.process.optimization_status == OptimizationStatus.success:
-        bfs_success_count_SIM += 1
-        print("BFS SIM SUCCESS!")
+    dfs_pso_balanced_SIM = create_PSO_algorithm_DFS_balanced_SIM()
+    dfs_pso_balanced_SIM.run()
+    if dfs_pso_balanced_SIM.process.optimization_status == OptimizationStatus.success:
+        dfs_success_balanced_count_SIM += 1
+        print("DFS balanced SIM SUCCESS!")
+    dfs_pso_unbalanced_SIM = create_PSO_algorithm_DFS_unbalanced_SIM()
+    dfs_pso_unbalanced_SIM.run()
+    if dfs_pso_unbalanced_SIM.process.optimization_status == OptimizationStatus.success:
+        dfs_unbalanced_success_count_SIM += 1
+        print("DFS unbalanced SIM SUCCESS!")
 
-    dfs_pso_SEQ = create_PSO_algorithm_DFS_SEQ()
-    dfs_pso_SEQ.run()
-    if dfs_pso_SEQ.process.optimization_status == OptimizationStatus.success:
-        dfs_success_count_SEQ += 1
-        print("DFS SEQ SUCCESS!")
-    bfs_pso_SEQ = create_PSO_algorithm_BFS_SEQ()
-    bfs_pso_SEQ.run()
-    if bfs_pso_SEQ.process.optimization_status == OptimizationStatus.success:
-        bfs_success_count_SEQ += 1
-        print("BFS SEQ SUCCESS!")
+    dfs_pso_balanced_SEQ = create_PSO_algorithm_DFS_balanced_SEQ()
+    dfs_pso_balanced_SEQ.run()
+    if dfs_pso_balanced_SEQ.process.optimization_status == OptimizationStatus.success:
+        dfs_balanced_success_count_SEQ += 1
+        print("DFS balanced SEQ SUCCESS!")
+    dfs_pso_unbalanced_SEQ = create_PSO_algorithm_DFS_unbalanced_SEQ()
+    dfs_pso_unbalanced_SEQ.run()
+    if dfs_pso_unbalanced_SEQ.process.optimization_status == OptimizationStatus.success:
+        dfs_unbalanced_success_count_SEQ += 1
+        print("DFS unbalanced SEQ SUCCESS!")
     print("Iteracja:" + str(i))
 
 print(
@@ -191,7 +226,9 @@ print(
 print(str([MAX_CALLS, EXPECTED_QUALITY, len(INPUT_VECTOR), HOW_MANY_PARTICLES,
            HOW_MANY_TRIES]))
 #
-print("DFS SIM count:" + str(dfs_success_count_SIM))
-print("BFS SIM count:" + str(bfs_success_count_SIM))
-print("DFS SEQ count:" + str(dfs_success_count_SEQ))
-print("BFS SEQ count:" + str(bfs_success_count_SEQ))
+print("dfs_success_balanced_count_SIM" + str(dfs_success_balanced_count_SIM))
+print(
+    "dfs_unbalanced_success_count_SIM" + str(dfs_unbalanced_success_count_SIM))
+print("dfs_balanced_success_count_SEQ" + str(dfs_balanced_success_count_SEQ))
+print(
+    "dfs_unbalanced_success_count_SEQ:" + str(dfs_unbalanced_success_count_SEQ))
